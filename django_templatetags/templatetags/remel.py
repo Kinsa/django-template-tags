10b894669802d69ea72900c4a7a25603c532c5ed
Joe Bergantine <jbergantine@gmail.com>
2014-02-20 14:41:24 -0700

    Update remel.py

--- django_templatetags/templatetags/remel.py
+++ django_templatetags/templatetags/remel.py
@@ -9,12 +9,15 @@ register = Library()
 def remel(value, arg):
     if len(arg.split()) > 1:
         raise TemplateSyntaxError("The 'remel' filter takes exactly one argument, the name of the single HTML element to be removed from the output.")
+
     def removeEl(soup, elname):
         for el in soup.findAll(elname):
             el.extract()
 
     bs = BeautifulSoup(value)
+
     removeEl(bs, arg)
+
     return bs
 
 register.filter(remel)

af7da9c88bfcbe947d1e0a23f8e5b21aedc6dfcb
Joe Bergantine <jbergantine@gmail.com>
2014-02-20 14:39:52 -0700

    Spaces > Tabs

--- django_templatetags/templatetags/remel.py
+++ django_templatetags/templatetags/remel.py
@@ -7,14 +7,14 @@ register = Library()
 
 
 def remel(value, arg):
-	if len(arg.split()) > 1:
-		raise TemplateSyntaxError("The 'remel' filter takes exactly one argument, the name of the single HTML element to be removed from the output.")
-	def removeEl(soup, elname):
-	    for el in soup.findAll(elname):
-	        el.extract()
+    if len(arg.split()) > 1:
+        raise TemplateSyntaxError("The 'remel' filter takes exactly one argument, the name of the single HTML element to be removed from the output.")
+    def removeEl(soup, elname):
+        for el in soup.findAll(elname):
+            el.extract()
 
-	bs = BeautifulSoup(value)
-	removeEl(bs, arg)
-	return bs
+    bs = BeautifulSoup(value)
+    removeEl(bs, arg)
+    return bs
 
 register.filter(remel)
